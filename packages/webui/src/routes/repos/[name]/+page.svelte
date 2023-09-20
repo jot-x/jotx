822bde89d740c0379adcad7ba33769e463722ec9
Asaf Shakarzy <asaf@crossid.io>
2023-09-20 19:59:24 +0300

    feat: Add a new note should find next free "Untitled" name
    resolves #15

--- packages/webui/src/routes/repos/[name]/+page.svelte
+++ packages/webui/src/routes/repos/[name]/+page.svelte
@@ -10,9 +10,33 @@
 	async function onCreateNote() {
 		const { getDocStore } = await import('$lib/api/setup');
 		const { writeDoc } = await import('@jotx/api');
-		const doc = newNote({ path: '/Untitled.md', content: '' });
-		const note = await writeDoc(getDocStore(repo.name), { path: '/Untitled.md', doc });
+		const path = '/' + getNewFileName(data.docs);
+		const doc = newNote({ path, content: '' });
+		console.log(path);
+		const note = await writeDoc(getDocStore(repo.name), { path, doc });
 		await invalidate('notes:load');
+	}
+
+	function getNewFileName(objects: { meta: { path: string } }[]): string {
+		// Extract all "Untitled" names and store them in an array
+		const untitledNames: string[] = objects
+			.map((obj) => obj.meta.path)
+			.filter((path) => path.startsWith('/Untitled'));
+
+		// Find the highest numbered "Untitled" name
+		let maxNumber = 0;
+		untitledNames.forEach((path) => {
+			const match = path.match(/^\/Untitled_(\d+).md$/);
+			if (match) {
+				const number = parseInt(match[1]);
+				if (!isNaN(number) && number > maxNumber) {
+					maxNumber = number;
+				}
+			}
+		});
+
+		// Return the next available name
+		return `Untitled_${maxNumber + 1}.md`;
 	}
 </script>
 
