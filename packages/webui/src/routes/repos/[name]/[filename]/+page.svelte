b863147888aabfa3c98496a3bd44469f45f338c9
Asaf Shakarzy <asaf@crossid.io>
2023-09-20 19:10:33 +0300

    feat: warn user in beforeunload event if doc is not saved yet
    resolves #12

--- packages/webui/src/routes/repos/[name]/[filename]/+page.svelte
+++ packages/webui/src/routes/repos/[name]/[filename]/+page.svelte
@@ -3,19 +3,34 @@
 	import type { PageData } from './$types';
 	import Editor from '$lib/components/Editor.svelte';
 	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
+	let dirty = false;
 	$: filename = data.filename;
 	$: doc = data.doc;
 	let saving = false;
 
-	const handleInput = _debounce(async (e) => {
-		saving = true;
-		const { getDocStore } = await import('$lib/api/setup');
-		const { writeDoc } = await import('@jotx/api');
-		await writeDoc(getDocStore(data.repo_name), { path: filename, doc });
-		saving = false;
-	}, 2000);
+	const handleInput = async (e) => {
+		dirty = true;
+		_debounce(async (e) => {
+			saving = true;
+			const { getDocStore } = await import('$lib/api/setup');
+			const { writeDoc } = await import('@jotx/api');
+			await writeDoc(getDocStore(data.repo_name), { path: filename, doc });
+			saving = false;
+			dirty = false;
+		}, 2000)(e);
+	};
+
+	onMount(() => {
+		window.addEventListener('beforeunload', (e) => {
+			if (dirty) {
+				e.preventDefault();
+				e.returnValue = 'You have unsaved changes. Are you sure you want to leave?';
+			}
+		});
+	});
 </script>
 
 <div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
